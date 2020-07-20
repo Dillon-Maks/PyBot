64e88c8ef4e40288fe8ef577b4a192f232d4ee7a
Dillon Maks <Dillonmaks123@gmail.com>
2020-07-19 23:33:43 -0500

    Functioning XP Addition Implemented
    Fix adding new members to the JSON

--- Json Testing.py
+++ Json Testing.py
@@ -11,19 +11,18 @@ async def on_ready():
     print('{0.user} is connected.'.format(client))
 
 @client.command()
-async def xp(ctx, amount):
+async def xp(ctx, amount: int):
     author = str(ctx.author)
-    dumpData = {}
-    dumpData['users'] = []
-    dumpData['users'].append({
-        "Name": author,
-        "XP": amount
-    })
+    with open("./data.json") as json_file:
+        dumpData = json.load(json_file)
+    json_file.close()
+    print(dumpData)
 
-    with open("./data.json", "w") as jsonSave:
-        json.dump(dumpData, jsonSave)
+    for user in dumpData['users']:
+        if user["Name"] == str(ctx.author):
+            user["XP"] += amount
 
-
-
+    with open("./data.json", "w") as json_file:
+        json.dump(dumpData, json_file)
 
 client.run("NzMzNDY4ODA1Njk0NzUwNzQw.XxDmKQ.SAJEVE2YO6sXsZ5up7R6TinlYt8")
