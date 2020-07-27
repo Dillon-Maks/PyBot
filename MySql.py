8d63320dc387ef08b1e30a49074f449251697d3c
Dillon Maks <Dillonmaks123@gmail.com>
2020-07-27 12:56:16 -0500

    Implemented Level System

--- MySql.py
+++ MySql.py
@@ -22,6 +22,15 @@ async def on_message(ctx):
             c.execute("SELECT * FROM users WHERE UserID=?", [str(ctx.author)])
             user = c.fetchone()
         newXP = user[2] + random.randrange(1, 3)
+
+        if newXP > round(100 * pow(user[1], 1.2)):
+            currentLvl = user[1] + 1
+            c.execute("UPDATE users SET Level=? Where UserID=?", (currentLvl, str(ctx.author)))
+            conn.commit()
+        elif newXP < 100:
+            c.execute("UPDATE users set Level=1 WHERE UserID=?", [str(ctx.author)])
+            conn.commit()
+
         c.execute("UPDATE users SET XP=? WHERE UserID=?", (newXP, str(ctx.author)))
         conn.commit()
         await client.process_commands(ctx)
@@ -30,7 +39,7 @@ async def on_message(ctx):
 async def xpcheck(ctx):
     c.execute("SELECT * FROM users WHERE UserID=?", [str(ctx.author)])
     user = c.fetchone()
-    await ctx.send(str(user[2]) + "/" + str(user[1]))
+    await ctx.send("Level: " + str(user[1]) + "\n" + str(user[2]) + "/" + str(round(100 * pow(user[1], 1.2))))
 
 
 client.run('NzMzNDY4ODA1Njk0NzUwNzQw.XxDmKQ.SAJEVE2YO6sXsZ5up7R6TinlYt8')
