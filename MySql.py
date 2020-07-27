fe0fcccdb3226268f3c1c3025b1b495fa75b24cd
Dillon Maks <Dillonmaks123@gmail.com>
2020-07-26 19:30:35 -0500

    XP Check Function
    XP Check added, need to check if new user works

--- MySql.py
+++ MySql.py
@@ -1,20 +1,31 @@
 import sqlite3
+from discord.ext import commands
 
-
+client = commands.Bot(command_prefix='*')
 conn = sqlite3.connect('users.db')
 
 c = conn.cursor()
 
-# c.execute("""CREATE TABLE users (
-#            UserID text,
-#            Level integer,
-#            XP integer,
-#            Currency integer
-#            )""")
-# c.execute("INSERT INTO users VALUES ('Maksimum#6716', 0, 0, 0)")
+@client.event
+async def on_ready():
+    print('{0.user} is connected'.format(client))
 
-c.execute("SELECT * FROM users WHERE UserID='Maksimum#6716'")
-print(c.fetchone())
+@client.command()
+async def xpcheck(ctx):
+    user = c.execute("SELECT * FROM users WHERE UserID=?", [str(ctx.author)])
+    if user is None:
+        c.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (str(ctx.author), 0, 0, 0))
+        conn.commit()
+        text = c.fetchone()
+        await ctx.send(c.fetchone())
+    else:
+        text = c.fetchone()
+        await ctx.send(str(text[2]))
+    conn.commit()
 
-conn.commit()
-conn.close()
+client.run('NzMzNDY4ODA1Njk0NzUwNzQw.XxDmKQ.SAJEVE2YO6sXsZ5up7R6TinlYt8')
+
+
+
+
+
