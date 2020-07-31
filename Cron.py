5d9063008b8b5474b899593a36b783c1f1007050
Dillon Maks <Dillonmaks123@gmail.com>
2020-07-30 21:51:01 -0500

    Python-Crontab Fix

--- Cron.py
+++ Cron.py
@@ -1,4 +1,10 @@
 from crontab import CronTab
 
+cron = CronTab(user=True)
+job = cron.new(command='echo Hello World!')
+job.minute.every(1)
+
+cron.write()
+
 
 token = 'NzMzNDY4ODA1Njk0NzUwNzQw.XxDmKQ.SAJEVE2YO6sXsZ5up7R6TinlYt8'
